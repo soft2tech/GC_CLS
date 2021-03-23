b9344f471c5c422ff92748e5b3b7fce792a51215
geekyChunck <ermia1303@gmail.com>
2021-03-23 23:55:40 +0430

    0.2

--- app.py
+++ app.py
@@ -28,24 +28,33 @@ def open_class(url, UN, passwd):
         driver.find_element_by_id("pwd").send_keys(str(passwd))
         driver.find_element_by_xpath("//input[@id='login-button']") .click()
         driver.find_element_by_xpath("//button[@class='button-style']") .click()
-        exit()
-get_time()
+        while True:
+            get_time()
+            if TM == ftime:
+                break
+
 while True:
+    get_time()
     if WD == "Sat":
         dic = DIC['sat']
-        if 758 <= int(TM) <= 858:
+        if 758 <= int(TM) <= 830:
+            ftime = 900
             url = dic['1st']
             open_class(url, ID, PD)
-        elif 918 <= int(TM) <= 1018:
+        elif 919 <= int(TM) <= 1000:
+            ftime = 1020
             url = dic['2nd']
             open_class(url, ID, PD)
-        elif 1038 <= int(TM) <= 1138:
+        elif 1039 <= int(TM) <= 1100:
+            ftime = 1140
             url = dic['3rd']
             open_class(url, ID, PD)
-        elif 1158 <= int(TM) <= 1258:
+        elif 1159 <= int(TM) <= 1220:
+            ftime = 1300
             url = dic['4th']
             open_class(url, ID, PD)
-        elif 1318 <= int(TM) <= 1418:
+        elif 1319 <= int(TM) <= 1340:
+            ftime = 1400
             url = dic['5th']
             open_class(url, ID, PD)
         else:
@@ -54,18 +63,23 @@ while True:
     elif WD == "Sun":
         dic = DIC['sun']
         if 758 <= int(TM) <= 858:
+            ftime = 900
             url = dic['1st']
             open_class(url, ID, PD)
         elif 918 <= int(TM) <= 1018:
+            ftime = 1020
             url = dic['2nd']
             open_class(url, ID, PD)
         elif 1038 <= int(TM) <= 1138:
+            ftime = 1140
             url = dic['3rd']
             open_class(url, ID, PD)
         elif 1158 <= int(TM) <= 1258:
+            ftime = 1300
             url = dic['4th']
             open_class(url, ID, PD)
         elif 1318 <= int(TM) <= 1418:
+            ftime = 1400
             url = dic['5th']
             open_class(url, ID, PD)
         else:
@@ -74,18 +88,23 @@ while True:
     elif WD == "Mon":
         dic = DIC['mon']
         if 758 <= int(TM) <= 858:
+            ftime = 900
             url = dic['1st']
             open_class(url, ID, PD)
         elif 918 <= int(TM) <= 1018:
+            ftime = 1020
             url = dic['2nd']
             open_class(url, ID, PD)
         elif 1038 <= int(TM) <= 1138:
+            ftime = 1140
             url = dic['3rd']
             open_class(url, ID, PD)
         elif 1158 <= int(TM) <= 1258:
+            ftime = 1300
             url = dic['4th']
             open_class(url, ID, PD)
         elif 1318 <= int(TM) <= 1418:
+            ftime = 1400
             url = dic['5th']
             open_class(url, ID, PD)
         else:
@@ -94,18 +113,23 @@ while True:
     elif WD == "Tue":
         dic = DIC['tue']
         if 758 <= int(TM) <= 858:
+            ftime = 900
             url = dic['1st']
             open_class(url, ID, PD)
         elif 918 <= int(TM) <= 1018:
+            ftime = 1020
             url = dic['2nd']
             open_class(url, ID, PD)
         elif 1038 <= int(TM) <= 1138:
+            ftime = 1140
             url = dic['3rd']
             open_class(url, ID, PD)
         elif 1158 <= int(TM) <= 1258:
+            ftime = 1300
             url = dic['4th']
             open_class(url, ID, PD)
         elif 1318 <= int(TM) <= 1418:
+            ftime = 1400
             url = dic['5th']
             open_class(url, ID, PD)
         else:
@@ -114,18 +138,23 @@ while True:
     elif WD == "Wed":
         dic = DIC['wed']
         if 758 <= int(TM) <= 858:
+            ftime = 900
             url = dic['1st']
             open_class(url, ID, PD)
         elif 918 <= int(TM) <= 1018:
+            ftime = 1020
             url = dic['2nd']
             open_class(url, ID, PD)
         elif 1038 <= int(TM) <= 1138:
+            ftime = 1140
             url = dic['3rd']
             open_class(url, ID, PD)
         elif 1158 <= int(TM) <= 1258:
+            ftime = 1300
             url = dic['4th']
             open_class(url, ID, PD)
         elif 1318 <= int(TM) <= 1418:
+            ftime = 1400
             url = dic['5th']
             open_class(url, ID, PD)
         else:
